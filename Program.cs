3846fd6c3ef85dc1388c3ebfca5576ebf676e321
Valeria <valeriakuz92@mail.ru>
2022-11-25 22:17:18 +0500

    Сохранение задачи 23

--- Program.cs
+++ Program.cs
@@ -28,28 +28,43 @@
 
 // Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 
-Console.WriteLine("Enter coordinates point A: ");
-Console.WriteLine("Enter Xa: ");
-double Xa = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter coordinates point A: ");
+// Console.WriteLine("Enter Xa: ");
+// double Xa = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Enter Ya: ");
-double Ya = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter Ya: ");
+// double Ya = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Enter Za: ");
-double Za = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter Za: ");
+// double Za = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Enter coordinates point B: ");
-Console.WriteLine("Enter Xb: ");
-double Xb = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter coordinates point B: ");
+// Console.WriteLine("Enter Xb: ");
+// double Xb = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Enter Yb: ");
-double Yb = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter Yb: ");
+// double Yb = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Enter Zb: ");
-double Zb = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter Zb: ");
+// double Zb = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine($"A: ({Xa},{Ya},{Za}), B: ({Xb},{Yb},{Zb})");
+// Console.WriteLine($"A: ({Xa},{Ya},{Za}), B: ({Xb},{Yb},{Zb})");
 
-double Lenth = Math.Sqrt((Xa-Xb)*(Xa-Xb) + (Ya-Yb)*(Ya-Yb) +
-(Za-Zb)*(Za-Zb));
-Console.WriteLine($"Lenth AB = {Lenth} ");
+// double Lenth = Math.Sqrt((Xa-Xb)*(Xa-Xb) + (Ya-Yb)*(Ya-Yb) +
+// (Za-Zb)*(Za-Zb));
+// Console.WriteLine($"Lenth AB = {Lenth} ");
+
+// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+
+Console.WriteLine("Enter the number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+int cube = 1;
+while (count <= number)
+{
+cube =Convert.ToInt32( Math.Pow (count, 3));
+Console.WriteLine($"The cube of number {count} is: {cube}");
+count++;
+}
